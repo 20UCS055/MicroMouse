0e37f71e2a2bfd3ec6722b34996f01ce7718f42a
Adarsh <adarshhero360@gmail.com>
2023-07-17 13:32:27 +0530

    Updates

---
+++ precache-manifest.e0258defae6678cbc651f23fe96e62f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6e648b0afb4f781b1734c3439accc3",
+    "url": "/MicroMouse/index.html"
+  },
+  {
+    "revision": "7e663743a7c996123297",
+    "url": "/MicroMouse/static/css/main.267238ff.chunk.css"
+  },
+  {
+    "revision": "3f51ea05862ffd4db38d",
+    "url": "/MicroMouse/static/js/2.f0583ba6.chunk.js"
+  },
+  {
+    "revision": "7e663743a7c996123297",
+    "url": "/MicroMouse/static/js/main.fada2c45.chunk.js"
+  },
+  {
+    "revision": "bedc9c103fee4009bb36",
+    "url": "/MicroMouse/static/js/runtime-main.d8ba8b5e.js"
+  }
+]);
